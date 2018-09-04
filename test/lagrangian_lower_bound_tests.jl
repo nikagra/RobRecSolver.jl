2c48b27e68e2ffc6c714ea71726401f420381acb
Mikita Hradovich <mikita.hradovich@pwr.edu.pl>
2018-09-04 21:30:46 +0200

    Unit tests fix

--- test/lagrangian_lower_bound_tests.jl
+++ test/lagrangian_lower_bound_tests.jl
@@ -9,5 +9,5 @@ end
 begin
     α = 0.5
     l = 2 * (1 - α)
-    @time @test lagrangianLowerBound([2 3; 1 4], [2 2; 3 1], [5 4; 3 4], 6, getAssignmentConstraints(2), [0 1; 1 0], l) == 11
+    @time @test lagrangianLowerBound([2 3; 1 4], [2 2; 3 1], [5 4; 3 4], 6, getAssignmentConstraints(2), l) == 11
 end
