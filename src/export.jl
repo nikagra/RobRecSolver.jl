c94fd2374dbbcb8db2aa5b8b17373b6420b260a3
Mikita Hradovich <mikita.hradovich@pwr.edu.pl>
2018-12-13 18:44:11 +0100

    Final changes in experiments

--- src/export.jl
+++ src/export.jl
@@ -18,9 +18,9 @@ function exportKnapsackResults(n, limit, αs, results)
         saveCsv("kn-plot-ratios-adv_rec_sel-$n-$suffix.csv", df, ["α", "ρ_Adv",  "ρ_h",  "ρ_Sel"])
         drawAndSavePlot("kn-plot-ratios-adv_rec_sel-$n-$suffix.pdf", αs, results[2:4, 1, :].', "α", "average ratio ρₖ", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}"])
 
-        df = DataFrame(a = αs, ra = results[2, 2, :], rh = results[3, 2, :], rs = results[4, 2, :], re = results[5, 2, :])
+        df = DataFrame(a = αs, ra = results[2, 2, :], rh = results[3, 2, :], rs = results[4, 2, :])
         saveCsv("kn-plot-times-adv_rec_sel-$n-$suffix.csv", df, ["α", "ρ_Adv",  "ρ_h",  "ρ_Sel", "Eval."])
-        drawAndSavePlot("kn-plot-times-adv_rec_sel-$n-$suffix.pdf", αs, results[2:5, 2, :].', "α", "average time (s)", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}" "Eval."])
+        drawAndSavePlot("kn-plot-times-adv_rec_sel-$n-$suffix.pdf", αs, results[2:4, 2, :].', "α", "average time (s)", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}"])
     end
 end
 
@@ -44,9 +44,9 @@ function exportAssignmentResults(m, limit, αs, results)
         saveCsv("as-plot-ratios-adv_rec_sel-$m-$suffix.csv", df, ["α", "ρ_Adv",  "ρ_h",  "ρ_Sel", "ρ_Lag"])
         drawAndSavePlot("as-plot-ratios-adv_rec_sel-$m-$suffix.pdf", αs, results[2:5, 1, :].', "α", "average ratio ρₖ", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}" L"\rho_{Lag}"])
 
-        df = DataFrame(a = αs, ra = results[2, 2, :], rh = results[3, 2, :], rs = results[4, 2, :], rl = results[5, 2, :], re = results[6, 2, :])
-        saveCsv("as-plot-times-adv_rec_sel-$m-$suffix.csv", df, ["α", "ρ_Adv",  "ρ_h",  "ρ_Sel", "ρ_Lag", "Eval."])
-        drawAndSavePlot("as-plot-times-adv_rec_sel-$m-$suffix.pdf", αs, results[2:6, 2, :].', "α", "average time (s)", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}" L"\rho_{Lag}" "Eval."])
+        df = DataFrame(a = αs, ra = results[2, 2, :], rh = results[3, 2, :], rs = results[4, 2, :], rl = results[5, 2, :])
+        saveCsv("as-plot-times-adv_rec_sel-$m-$suffix.csv", df, ["α", "ρ_Adv",  "ρ_h",  "ρ_Sel", "ρ_Lag"])
+        drawAndSavePlot("as-plot-times-adv_rec_sel-$m-$suffix.pdf", αs, results[2:5, 2, :].', "α", "average time (s)", [L"\rho_{Adv}" L"\rho_h" L"\rho_{Sel}" L"\rho_{Lag}"])
     end
 end
 
@@ -56,6 +56,6 @@ function saveCsv(filename, data, columnNames)
 end
 
 function drawAndSavePlot(filename, x, ys, xlabel, ylabel, yslabels; linewidth=2, linestyles = [:solid :dash :dashdot :dot :solid], shape = [:diamond :pentagon :star4 :utriangle :square], palette=cgrad([:black, :grey]), annotations = [])
-    p = plot(x, ys, linewidth=linewidth, xlabel = xlabel, ylabel = ylabel, lab = yslabels, linestyle = linestyles, shape = shape, palette = palette, annotations = annotations)
+    p = plot(x, ys, linewidth=linewidth, xlabel = xlabel, ylabel = ylabel, lab = yslabels, linestyle = linestyles, shape = shape, markersize = 8, palette = palette, annotations = annotations)
     savefig(p, filename)
 end
